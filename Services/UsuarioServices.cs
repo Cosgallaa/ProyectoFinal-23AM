b0f1c6bfa6b5a28841ab8482a9bf4a1a6fc54985
Cosgallaa <acjosecgrey@gmail.com>
2023-07-16 23:37:40 -0500

    Agregar archivos de proyecto.

---
+++ Services/UsuarioServices.cs
@@ -0,0 +1,127 @@
+﻿using ProyectoFinal_23AM.Context;
+using ProyectoFinal_23AM.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProyectoFinal_23AM.Services
+{
+    public class UsuarioServices
+    {
+        public void AddUser(Usuario request)
+        {
+            try
+            {
+                if (request != null)
+                {
+                    using (var _context = new AppDBConttext())
+                    {
+                        Usuario res = new Usuario()
+                        {
+                            Nombre = request.Nombre,
+                            UserName = request.UserName,
+                            Password = request.Password,
+                            // FkRol = request.FkRol,
+                        };
+                        _context.Usuarios.Add(res);
+                        _context.SaveChanges();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocurrió un error "+ ex.Message);
+            }
+        }
+       
+        public void UpdateUser(Usuario request)
+        {
+            try
+            {
+               
+                    using (var _context = new AppDBConttext())
+                    {
+                    Usuario update = _context.Usuarios.Find(request.PkUsuario);
+                    Usuario res = new Usuario();
+
+
+                        update.Nombre = request.Nombre;
+                        update.UserName = request.UserName;
+                        update.Password = request.Password;
+                            // FkRol = request.FkRol,
+                    
+                        _context.Usuarios.Update(update);
+                        _context.SaveChanges();
+                    }
+                
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocurrió un error " + ex.Message);
+            }
+        }
+        public void DeleteUser(int UserId)
+        {
+            try
+            {
+                using (var _context = new AppDBConttext())
+                {
+                    Usuario usuario = _context.Usuarios.Find(UserId);
+                    if (usuario!= null)
+                    {
+                        _context.Remove(usuario);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        MessageBox.Show("NO HAY REGISTRO");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("ERROR: " + ex.Message);
+            }
+        }
+        public List<Usuario> GetUsuarios()
+        {
+            try
+            {
+                using (var _context = new AppDBConttext())
+                {
+                    List<Usuario> usuarios = new List<Usuario>();
+
+                    usuarios = _context.Usuarios.ToList();
+
+                    return usuarios;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Sucedió un error " + ex.Message);
+            }
+        }
+        public List<Rol> GetRoles()
+        {
+            try
+            {
+                using (var _context = new AppDBConttext())
+                {
+                    List<Rol> roles = new List<Rol>();
+
+                    roles = _context.Roles.ToList();
+
+                    return roles;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Sucedió un error " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Vistas/Sistema.xaml.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Exchange.WebServices.Data;
+using ProyectoFinal_23AM.Entities;
+using ProyectoFinal_23AM.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinal_23AM.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para Sistema.xaml
+    /// </summary>
+    public partial class Sistema : Window
+    {
+        public Sistema()
+        {
+            InitializeComponent();
+            GetUserTable();
+            GetRoles();
+        }
+
+        UsuarioServices services = new UsuarioServices();
+        //Este boton tiene 2 funciones, (CUARDAR/EDITAR)
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //FUNCION GUARDAR
+            if(txtPkUser.Text == "")
+            {
+                Usuario usuario = new Usuario()
+                {
+                    Nombre = txtNombre.Text,
+                    UserName = txtUserName.Text,
+                    Password = txtPassword.Text
+                };
+
+                services.AddUser(usuario);
+                MessageBox.Show("Usuario agregado");
+                txtNombre.Clear();
+                txtUserName.Clear();
+                txtPassword.Clear();
+                GetUserTable();
+            }
+            //FUNCION EDITAR
+            else
+            {
+                int userId = Convert.ToInt32(txtPkUser.Text);
+
+                Usuario usuario = new Usuario()
+                {
+                    PkUsuario = userId,
+                    Nombre = txtNombre.Text,
+                    UserName = txtUserName.Text,
+                    Password = txtPassword.Text
+                };
+                MessageBox.Show("Usuario actualizado");
+                services.UpdateUser(usuario);
+                txtPkUser.Clear();
+                txtNombre.Clear();
+                txtUserName.Clear();
+                txtPassword.Clear();
+                GetUserTable();
+                //hacer funcion editar y eliminar
+            }
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int userId = Convert.ToInt32(txtPkUser.Text);
+            Usuario usuario = new Usuario();
+            usuario.PkUsuario = userId;
+            services.DeleteUser(userId);
+            MessageBox.Show("Registro Eliminado");
+
+        }
+        public void EditItem(object sender, RoutedEventArgs e)
+        {
+            Usuario usuario = new Usuario();
+
+            usuario = (sender as FrameworkElement).DataContext as Usuario;
+            txtPkUser.Text = usuario.PkUsuario.ToString();
+            txtNombre.Text = usuario.Nombre.ToString();
+            txtUserName.Text = usuario.UserName.ToString();
+            txtPassword.Text = usuario.Password.ToString();
+        }
+        public void GetUserTable()
+        {
+            UserTable.ItemsSource = services.GetUsuarios();
+        }
+        public void GetRoles()
+        {
+            SelectRol.ItemsSource = services.GetRoles();
+            SelectRol.DisplayMemberPath = "Nombre";
+            SelectRol.SelectedValuePath = "PkRol";
+        }
+
+    }
+}
